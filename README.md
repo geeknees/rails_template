56fffc795babe5acaf269de6492f71600bf56f6a
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2019-09-18 19:57:36 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Usage
+
+
+```bash
+rails new your_project_name -d postgresql --webpack=react -m template.rb
+```
+
+```
+rails new your_project_name -d postgresql --webpack=react --skip-test --skip-turbolinks -m template.rb
+
+```
+
+### API
+
+```bash
+rails new your_project_name -d postgresql --webpack=react --api -m template.rb
+```
+
+```
+rails new your_project_name -d postgresql --webpack=react --api --skip-test --skip-turbolinks -m template.rb
+```
+
+## LICENCE
+
+MIT
---
+++ template.rb
@@ -0,0 +1,44 @@
+after_bundle do
+  git :init
+  git add: "."
+  git commit: %Q{ -m 'Initial commit' }
+end
+
+gem 'clockwork'
+gem 'hamlit'
+gem 'high_voltage'
+gem 'rack-cors'
+gem 'rack-rewrite'
+gem 'rack-user_agent'
+gem 'seed-fu'
+gem 'sidekiq'
+
+gem_group :development, :test do
+  gem 'awesome_print'
+  gem 'pry-byebug'
+  gem 'pry-doc'
+  gem 'pry-rails'
+  gem 'rspec-rails'
+end
+
+gem_group :test do
+  gem 'capybara'
+  gem 'factory_bot_rails'
+  gem 'faker'
+  gem 'selenium-webdriver'
+  gem 'shoulda-matchers'
+  gem 'timecop'
+end
+
+gem_group :development do
+  gem 'annotate'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'bullet'
+  gem 'letter_opener'
+  gem 'neovim'
+  gem 'rack-mini-profiler'
+  gem 'rails_best_practices'
+  gem 'refe2'
+  gem 'solargraph'
+end
